7cb9e3f97417a341a8aa23313de250617f88d396
Keisuke Sehara <keisuke.sehara@gmail.com>
2019-12-14 18:11:47 +0100

    draft layout

--- src/main/java/MinimalVC.java
+++ src/main/java/MinimalVC.java
@@ -3,16 +3,103 @@
  */
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class MinimalVC {
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
+// may not needed in the future
+import javax.swing.BorderFactory;
+import javax.swing.JTree;
+import javax.swing.JTextField;
+import javax.swing.JList;
+
+public class MinimalVC
+    extends JPanel
+{
     static final String TITLE_EMPTY = "(MinimalVC) <no repository selected>";
 
-    public static void main(String[] args) {
+    JSplitPane content_;
+
+    public MinimalVC()
+    {
+        super(new BorderLayout());
+        setupUI();
+    }
+
+    /**
+     *  initializes itself with the default layout.
+     */
+    private void setupUI()
+    {
+        // add tree view for directory view
+        JTree directoryView         = new JTree();
+
+        // right side will be the control panel
+        JPanel controlPanel         = new JPanel();
+
+        GridBagLayout       layout  = new GridBagLayout();
+        GridBagConstraints  cons    = new GridBagConstraints();
+        controlPanel.setLayout(layout);
+
+        cons.weightx = 1.0;
+        cons.gridx   = 1;
+        cons.fill    = GridBagConstraints.BOTH;
+
+        // add unstaged changes view
+        JPanel unstagedChangesView  = new JPanel();
+        unstagedChangesView.setBorder(
+            BorderFactory.createTitledBorder("Unstaged changes"));
+        cons.gridheight = 1;
+        cons.weighty    = 1.0;
+        layout.setConstraints(unstagedChangesView, cons);
+        controlPanel.add(unstagedChangesView);
+
+        // add staged changes view
+        JPanel stagedChangesView    = new JPanel();
+        stagedChangesView.setBorder(
+            BorderFactory.createTitledBorder("Staged changes"));
+        layout.setConstraints(stagedChangesView, cons);
+        controlPanel.add(stagedChangesView);
+
+        // add commit message field
+        JTextField commitMessageField   = new JTextField();
+        commitMessageField.setBorder(
+            BorderFactory.createTitledBorder("Commit to repository"));
+        cons.weighty = 0.5;
+        layout.setConstraints(commitMessageField, cons);
+        controlPanel.add(commitMessageField);
+
+        // add commit log
+        JPanel commitLogView            = new JPanel();
+        commitLogView.setBorder(
+            BorderFactory.createTitledBorder("Commit log"));
+        cons.weighty = 1.0;
+        layout.setConstraints(commitLogView, cons);
+        controlPanel.add(commitLogView);
+
+        content_ = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+                                directoryView,
+                                controlPanel);
+        add(content_, BorderLayout.CENTER);
+    }
+
+    public void resetVerticalSplit() {
+        content_.setDividerLocation(0.5);
+    }
+
+    public static void main(String[] args)
+    {
+        MinimalVC content = new MinimalVC();
         JFrame frame = new JFrame();
-        frame.setSize(600, 400);
+        frame.setSize(600, 600);
         frame.setLocation(100, 100);
+        frame.setContentPane(content);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle(TITLE_EMPTY);
         frame.setVisible(true);
+        content.resetVerticalSplit();
     }
 }
